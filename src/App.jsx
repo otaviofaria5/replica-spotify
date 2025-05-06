28b18f8c955d2ec3c3826e87c5be62be6f489cf5
otaviofaria5 <otavio.faria@elumini-it.com>
2025-05-06 16:28:57 -0300

    Incial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
