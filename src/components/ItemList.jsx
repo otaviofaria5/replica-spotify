ec36e7b0c4cdd998096af7b2a2326bd2c1f334ab
otaviofaria5 <otavio.faria@elumini-it.com>
2025-05-08 10:45:49 -0300

    Parte 2

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import SingleItem from "./SingleItem";
+import { Link } from "react-router-dom";
+
+const ItemList = ({ title, items, itemsArry, path, idPath }) => {
+  return (
+    <div className="item-list">
+      <div className="item-list__header">
+        <h2>{title} populares</h2>
+        <Link to={path} className="item-list__link">
+          {" "}
+          Mostrar tudo
+        </Link>
+      </div>
+
+      <div className="item-list__container">
+        {itemsArry
+          .filter((currentValue, index) => index < items)
+          .map((currObj, index) => (
+            <SingleItem
+              // id={currObj.id}
+              //name={currObj.name}
+              //image={currObj.image}
+              // banner={currObj.banner}
+              idPath={idPath}
+              {...currObj}
+              key={`${title}-${index}`}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default ItemList;
