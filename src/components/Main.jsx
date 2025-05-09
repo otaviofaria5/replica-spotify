03150b40edebfaf1bcee5e2bd3e581022f54fb5d
otaviofaria5 <otavio.faria@elumini-it.com>
2025-05-09 09:32:09 -0300

    Parte 3

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,24 +3,34 @@ import ItemList from "./ItemList";
 import { artistArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
-const Main = () => {
+const Main = ({ type }) => {
   return (
     <div className="main">
-      <ItemList
-        title="Artistas"
-        items={5}
-        itemsArry={artistArray}
-        path="/artists"
-        idPath="/artist"
-      />
+      {/* Item List de Artistas */}
+      {type === "artists" || type === undefined ? (
+        <ItemList
+          title="Artistas"
+          items={10}
+          itemsArray={artistArray}
+          path="/artists"
+          idPath="/artist"
+        />
+      ) : (
+        <></>
+      )}
 
-      <ItemList
-        title="Músicas"
-        items={10}
-        itemsArry={songsArray}
-        path="/songs"
-        idPath="/song"
-      />
+      {/* Item List de Músicas */}
+      {type === "songs" || type === undefined ? (
+        <ItemList
+          title="Músicas"
+          items={20}
+          itemsArray={songsArray}
+          path="/songs"
+          idPath="/song"
+        />
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
