b236b26bc587b5c1af3ad00affcafd3ac9f33ace
otaviofaria5 <otavio.faria@elumini-it.com>
2025-05-09 19:34:46 -0300

    Ajustes Final

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -6,16 +6,22 @@ const app = express();
 const PORT = 3001;
 
 app.use(cors());
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static("../../front-end/dist"));
+
+app.get("*", (request, response) => {
+  response.sendFile("../../front-end/dist/index.html");
 });
 
 app.listen(PORT, () => {
